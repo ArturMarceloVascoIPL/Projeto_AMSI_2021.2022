70a839ad65d0b5d6a14beee80ccc5cd2525a11ff
Vasco Silva <2200426@my.ipleiria.pt>
2021-11-21 17:24:23 +0000

    v0.3 - Menu de Navegação Inferior criado
    - Criado o menu de navegação inferior
    - Adicionados os ícones para os botões
    - Criados fragmentos 'placeholder' para as páginas do menu

--- FitWorkout/app/src/main/java/com/example/myapplication/MainActivity.java
+++ FitWorkout/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.myapplication.vistas.FragmentCalendar;
+import com.example.myapplication.vistas.FragmentHistory;
+import com.example.myapplication.vistas.FragmentHome;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
+
+    /**
+     * Instanciar os Fragmentos
+     */
+    FragmentHome fragmentHome = new FragmentHome();
+    FragmentCalendar fragmentCalendar = new FragmentCalendar();
+    FragmentHistory fragmentHistory = new FragmentHistory();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_nav_home);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.bottom_nav_calendar:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentCalendar).commit();
+                return true;
+
+            case R.id.bottom_nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentHome).commit();
+                return true;
+
+            case R.id.bottom_nav_history:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentHistory).commit();
+                return true;
+        }
+        return false;
     }
 }
