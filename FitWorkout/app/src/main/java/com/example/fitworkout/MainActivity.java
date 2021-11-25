936b04a4428337f9e89c1a07fc6f3c90b07cef64
Vasco Silva <2200426@ipleiria.pt>
2021-11-25 18:52:08 +0000

    v0.6 - Drawer e Bottom Nav (WIP)
    - NÃO TESTADO
    - Alterações para tentar resolver problemas (não acabado)

--- FitWorkout/app/src/main/java/com/example/fitworkout/MainActivity.java
+++ FitWorkout/app/src/main/java/com/example/fitworkout/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.fitworkout;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -24,7 +25,6 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     private FragmentManager fragmentManager;
 
     private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle toggle;
     private NavigationView navigationView;
 
     @Override
@@ -32,24 +32,35 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        fragmentManager = getSupportFragmentManager();
+        Toolbar toolbar = findViewById(R.id.toolbar);
 
+        setSupportActionBar(toolbar);
         drawerLayout = findViewById(R.id.navigationDrawer);
         navigationView = findViewById(R.id.navigationView);
 
-        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.start, R.string.close);
-
-        drawerLayout.addDrawerListener(toggle);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.open,
+                R.string.close);
+        
         toggle.syncState();
+        
+        drawerLayout.addDrawerListener(toggle);
+        carregarCabecalho();
 
-        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        fragmentManager = getSupportFragmentManager();
 
-        navigationView.setNavigationItemSelectedListener(this);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
         // Apresentar a página Home por defeito
         bottomNavigationView.setSelectedItemId(R.id.bottom_nav_home);
+    }
+
+    private void carregarCabecalho() {
+        //TODO: Fazer método(ver na ficha da aula)
     }
 
     @Override
